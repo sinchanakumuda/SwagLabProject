a91c10238118c1e793e216a31db98da557f8e0a6
sinch <sinch@Sinchana_21>
2026-08-04 10:23:50 +0530

    updated

--- orangehrm-automation/src/main/java/config_BaseClass/BaseClass.java
+++ orangehrm-automation/src/main/java/config_BaseClass/BaseClass.java
@@ -25,7 +25,7 @@ public class BaseClass {
 	PropertyFileUtility p=new PropertyFileUtility();
 	
 	//Launching the browser for cross browser testing
-	@Parameters("browser")
+	/*@Parameters("browser")
 	@BeforeClass()
 	public void beforClassLaunchB(String browser) throws IOException
 	{
@@ -40,8 +40,8 @@ public class BaseClass {
 		 sdriver=driver;
 		 System.out.println("launching browser");
 		 
-	}
-	/*
+	}*/
+	
 	@BeforeClass()
 	public void beforClassLaunchB() throws IOException
 	{
@@ -56,7 +56,7 @@ public class BaseClass {
 		 sdriver=driver;
 		 System.out.println("launching browser");
 		 
-	}*/
+	}
 	
 	
 	//Method using Command line parameter
