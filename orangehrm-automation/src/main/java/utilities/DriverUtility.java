c1fa28d8f34cf4f54834dafc8f70034ef9ac8701
sinch <sinch@Sinchana_21>
2026-08-04 13:13:21 +0530

    commit

--- orangehrm-automation/src/main/java/utilities/DriverUtility.java
+++ orangehrm-automation/src/main/java/utilities/DriverUtility.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,34 +11,59 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.support.ui.Select;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverUtility {
 	
 	
-	    public static WebDriver getDriver(String browser) {
-	        if(browser.equalsIgnoreCase("chrome")) {
-	    		// Avoid Change Password Popup 
-	    		ChromeOptions settings = new ChromeOptions(); 
-	    		Map<String, Object> prefs = new HashMap<String, Object>(); 
-	    		prefs.put("profile.password_manager_leak_detection", false); 
-	    		settings.setExperimentalOption("prefs", prefs); 
-	            return new ChromeDriver(settings);
-	        } else if(browser.equalsIgnoreCase("edge")) {
-	            return new EdgeDriver();
-	        } else if(browser.equalsIgnoreCase("firefox")) {
-	            return new FirefoxDriver();
-	        }
-	        throw new IllegalArgumentException("Invalid browser: " + browser);
-	    }
+
 	    
+	    public static WebDriver getDriver(String browser) {
+	        WebDriver driver;
+
+	        if (browser.equalsIgnoreCase("chrome")) {
+	            WebDriverManager.chromedriver().setup();
+
+	            // Avoid Change Password Popup
+	            ChromeOptions settings = new ChromeOptions();
+	            Map<String, Object> prefs = new HashMap<>();
+	            prefs.put("profile.password_manager_leak_detection", false);
+	            settings.setExperimentalOption("prefs", prefs);
+
+	            driver = new ChromeDriver(settings);
+
+	        } else if (browser.equalsIgnoreCase("edge")) {
+	            WebDriverManager.edgedriver().setup();
+
+	            EdgeOptions options = new EdgeOptions();
+	            driver = new EdgeDriver(options);
+
+	        } else if (browser.equalsIgnoreCase("firefox")) {
+	            WebDriverManager.firefoxdriver().setup();
+
+	            FirefoxOptions options = new FirefoxOptions();
+	            driver = new FirefoxDriver(options);
+
+	        } else {
+	            throw new IllegalArgumentException("Invalid browser: " + browser);
+	        }
+
+	        driver.manage().window().maximize();
+	        return driver;
+	    }
 	    public static void maximizeWindow(WebDriver driver)
 		   {
 		    driver.manage().window().maximize();
